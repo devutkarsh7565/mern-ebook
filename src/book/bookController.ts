0d8bfdde6f93ad13b6e582c43ba15e5678ba4bc8
devutkarsh7565 <utkarshtiwari200389@gmail.com>
2024-06-12 04:40:34 +0530

    add author id to new book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -4,6 +4,7 @@ import path from "node:path";
 import createHttpError from "http-errors";
 import bookModel from "./bookModel";
 import fs from "node:fs";
+import { AuthRequest } from "../middlewares/authenticate";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   console.log("files", req.files);
@@ -48,10 +49,12 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
 
     console.log("upload result", uploadResult, bookFileUploadResult);
 
+    const _req = req as AuthRequest;
+
     const newBook = await bookModel.create({
       title,
       genre,
-      author: "66682917bd4849df2f89dabd",
+      author: _req.userId,
       coverImage: uploadResult.secure_url,
       file: bookFileUploadResult.secure_url,
     });
